wave units and fix up plot

SpecFit/SpecFit.py
@@ -16,12 +16,19 @@ FF_MODEL_KEYS = ('free_free', 'ff')
 
 class SpecFit:
 
-    def __init__(self, data: str | np.ndarray, *args, **kwargs):
+    def __init__(self, data: str | np.ndarray, wave_units=None,
+                 *args, **kwargs):
         if isinstance(data, str):
             self.data = read(data)
         else:
             # data = data.copy()
             self.data = data
+
+        if wave_units is None:
+            wave_units = 'angstroms'
+
+        if wave_units == 'microns':
+            self.data[:, 0] *= 1.e4
 
         kwargs['normalize'] = True
         self.data = preprocess(self.data, *args, **kwargs)
@@ -77,7 +84,7 @@ class SpecFit:
         return fit_report
 
     def plot(self, *args, **kwargs):
-        return plot_spectrum(data=self.data, model_result=self.result,
+        return plot_spectrum(self.data, model_result=self.result,
                              *args, **kwargs)
 
     def _parse_model(self, model: str | Callable) -> Callable:

SpecFit/util/default_params.py
@@ -31,7 +31,7 @@ def default_T_ff(data: np.ndarray, *args, **kwargs) -> dict:
 
     param_info = {
         'value': T,
-        'min': 3000.,
+        'min': 0.,
         'max': 20000.,
     }
 

examples/example1_simple.py
@@ -2,14 +2,14 @@ import numpy as np
 from SpecFit.SpecFit import SpecFit
 
 
-# Get spectrum data if file has unusual format
+# Manually read spectrum data if file has unusual format
 fn = 'sn2023ixf_visit1_combined_v3.dat'
 data = np.loadtxt(fn, usecols=(0, 2, 3), skiprows=1)
-data[:, 0] *= 1e4
 
 # Load and preprocess spectrum
 read_params = {
     'z': 0.0008,
+    'wave_units': 'microns',
 }
 spec = SpecFit(data, **read_params)
 
@@ -21,4 +21,4 @@ spec.fit()
 spec.fit_report()
 
 fn = 'test_plot.png'
-spec.plot(out_filename=fn, plot_type='nir', residuals=True)
+spec.plot(out_filename=fn, residuals=True)

examples/example2_detailed.py
@@ -2,20 +2,20 @@ import numpy as np
 from SpecFit.SpecFit import SpecFit
 
 
-# Get spectrum data if file has unusual format
+# Manually read spectrum data if file has unusual format
 fn = 'sn2023ixf_visit1_combined_v3.dat'
 data = np.loadtxt(fn, usecols=(0, 2, 3), skiprows=1)
-data[:, 0] *= 1e4
 
 # Load and preprocess spectrum
 read_params = {
     'z': 0.0008,
+    'wave_units': 'microns',
 }
 spec = SpecFit(data, **read_params)
 
 params_bb = {
     'T_planck': {
-        'value': 4000.,
+        'value': 6000.,
         'min': 4000.,
         'max': 10000.,
     },
@@ -28,16 +28,15 @@ spec.add_model('bb', params_bb)
 params_ff = {
     'T_ff': {
         'value': 6197.,
-        'min': 1000.,
+        'min': 0.,
         'max': 20000.,
-        # 'vary': False
     },
     'a_ff': {
         'value': 1.,
         'min': 0.,
     },
 }
-# spec.add_model('ff', params_ff)
+spec.add_model('ff', params_ff)
 
 fit_params = {
     # 'method': 'emcee',
@@ -47,4 +46,4 @@ spec.fit(**fit_params)
 spec.fit_report()
 
 fn = 'test_plot.png'
-spec.plot(out_filename=fn, plot_type='nir', residuals=True)
+spec.plot(out_filename=fn, residuals=True)